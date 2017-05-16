090f7718eaf04bdb00a40d386f5583d26425cd70
Edwin de Jonge <edwindjonge@gmail.com>
2017-05-16 23:36:09 +0200

    adding malta resizing

--- R/set_projection.R
+++ R/set_projection.R
@@ -8,11 +8,11 @@ for (f in list.files("../data",pattern = "europe_.+.shp", full.names = TRUE)){
   write_shape(shp, f)
 }
 
-world_eck4 <- read_shape("../data/world_eck4.shp", current.projection = get_proj4("eck4"))
-write_shape(world_eck4,"../data/world_eck4.shp")
+#world_eck4 <- read_shape("../data/world_eck4.shp", current.projection = get_proj4("eck4"))
+#write_shape(world_eck4,"../data/world_eck4.shp")
 
-world_robin <- read_shape("../data/world_robinson.shp", current.projection = get_proj4("robin"))
-write_shape(world_robin,"../data/world_robinson.shp")
+#world_robin <- read_shape("../data/world_robinson.shp", current.projection = get_proj4("robin"))
+#write_shape(world_robin,"../data/world_robinson.shp")
 
-world_longlat <- read_shape("../data/world_wgs84.shp", current.projection = get_proj4("longlat"))
-write_shape(world_longlat,"../data/world_wgs84.shp")
+#world_longlat <- read_shape("../data/world_wgs84.shp", current.projection = get_proj4("longlat"))
+#write_shape(world_longlat,"../data/world_wgs84.shp")
--- data-raw/eu_maps.bash
+++ data-raw/eu_maps.bash
@@ -1,8 +1,11 @@
-# curl -O http://ec.europa.eu/eurostat/cache/GISCO/geodatafiles/NUTS_2013_01M_SH.zip
-# curl -O http://ec.europa.eu/eurostat/cache/GISCO/geodatafiles/CNTR_2014_03M_SH.zip
 
-unzip -o NUTS_2013_01M_SH.zip
-unzip -o CNTR_2014_03M_SH.zip
+[ ! -e NUTS_2013_01M_SH.zip ] &&
+  curl -O http://ec.europa.eu/eurostat/cache/GISCO/geodatafiles/NUTS_2013_01M_SH.zip &&
+  unzip -o NUTS_2013_01M_SH.zip
+  
+[ ! -e CNTR_2014_03M_SH.zip ] &&
+  curl -O http://ec.europa.eu/eurostat/cache/GISCO/geodatafiles/CNTR_2014_03M_SH.zip &&
+  unzip -o CNTR_2014_03M_SH.zip
 
 rm -r ../data
 mkdir ../data
@@ -10,7 +13,7 @@ mkdir ../data
 mapshaper -i NUTS_2013_01M_SH/data/NUTS_RG_01M_2013.shp \
  -proj "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs" densify \
  -clip bbox=2500000,1400000,7350000,5750000 \
- -simplify 10% \
+ -simplify 10% keep-shapes\
  -split STAT_LEVL_ \
  -o ../data/europe_nuts.shp
 
@@ -19,7 +22,7 @@ mapshaper -i CNTR_2014_03M_SH/Data/CNTR_RG_03M_2014.shp \
  -clip bbox=-65,-25,80,70 \
  -proj "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs" densify \
  -clip bbox=2500000,1400000,7350000,5750000 \
- -simplify 10% \
+ -simplify 10% keep-shapes\
  -o ../data/europe_background.shp
 
 cd ../data
@@ -30,16 +33,7 @@ rename s/nuts3/nuts2/ europe_nuts3.*
 rename s/nuts4/nuts3/ europe_nuts4.*
 cd ../data-raw
 
-for proj in "wintri" "eck4" "kav7" "wgs84" "robinson"
-do
-  mapshaper -i CNTR_2014_03M_SH/Data/CNTR_RG_03M_2014.shp \
-   -filter 'CNTR_ID != "AQ"' \
-   -proj $proj densify \
-   -simplify 3% \
-   -o ../data/world_${proj}.json
-done
-
-mapshaper -i ../data/*.json -o ../data format=shapefile
+#mapshaper -i ../data/*.json -o ../data format=shapefile
 
 
 # save the projections
