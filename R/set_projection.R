7920f7c4132248407b038702b044fa3efd2a15f2
Edwin de Jonge <edwindjonge@gmail.com>
2017-05-19 13:41:47 +0200

    wip

--- R/set_projection.R
+++ R/set_projection.R
@@ -1,12 +1,19 @@
-data("Europe", package="tmap")
-library(tmaptools)
-
-proj <- get_projection(Europe)
-
-for (f in list.files("../data",pattern = "europe_.+.shp", full.names = TRUE)){
-  shp <- read_shape(f, current.projection = proj)
-  write_shape(shp, f)
+library(sf)
+for (f in list.files("../data",pattern = "europe_.+.geojson", full.names = TRUE)){
+  shp <- st_read(f)
+  st_crs(shp) <- 3035
+  st_write(shp, f, delete_dsn = T)
 }
+
+# data("Europe", package="tmap")
+# library(tmaptools)
+# 
+# proj <- get_projection(Europe)
+# 
+# for (f in list.files("../data",pattern = "europe_.+.shp", full.names = TRUE)){
+#   shp <- read_shape(f, current.projection = proj)
+#   write_shape(shp, f)
+# }
 
 #world_eck4 <- read_shape("../data/world_eck4.shp", current.projection = get_proj4("eck4"))
 #write_shape(world_eck4,"../data/world_eck4.shp")
--- malta.R
+++ malta.R
@@ -1,5 +1,5 @@
 library(sf)
-nuts0 <- st_read("data/europe_nuts0.shp")
+nuts0 <- st_read("data/europe_nuts0.geojson")
 
 scale_region <- function(sf, s=rep(1, nrow(sf))){
   center <- st_centroid(sf)
@@ -10,19 +10,3 @@ scale_region <- function(sf, s=rep(1, nrow(sf))){
   sf$geometry <- geom + center$geometry
   sf
 }
-
-for(sc in c(1,2,3,4,5)){
-  s <- ifelse(nuts0$NUTS_ID == "MT", sc, 1)
-  m <- 
-    nuts0 %>% 
-    scale_region(s) %>% 
-    tm_shape() + tm_fill(col="#00a1cd") + tm_borders(col="white") + tm_layout(paste0("Malta x ", sc)) 
-  print(m)
-}
-
-
-library(raster)
-r <- raster("~/Downloads/HDENS_CLST_2011/HDENS_CLST_2011.tif")
-plot(r)
-
-#tm_shape(nuts0) + tm_fill("gray90") + tm_shape(r) + tm_raster()
