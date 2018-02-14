854bbd0fa61bcad6b72d8430e4a486d944d50ba0
Edwin de Jonge <edwindjonge@gmail.com>
2018-02-14 23:01:06 +0100

    updated for wgs84

--- malta.R
+++ malta.R
@@ -2,11 +2,23 @@ library(sf)
 nuts0 <- st_read("data/europe_nuts0.geojson")
 
 scale_region <- function(sf, s=rep(1, nrow(sf))){
+  crs <- st_crs(sf)
   center <- st_centroid(sf)
   geom <- sf$geometry - center$geometry
   for (r in seq_len(nrow(sf))){
     geom[r] <- geom[r] * s[r]
   }
   sf$geometry <- geom + center$geometry
+  st_crs(sf) <- crs
   sf
 }
+
+
+# rescale Malta
+s <- ifelse(nuts0$NUTS_ID == "MT", sqrt(5), 1)
+
+nuts0_MT <- scale_region(nuts0, s)
+mapview::mapview(nuts0_MT)
+
+st_write(nuts0_MT, "data/europe_nuts0_MT.geojson", delete_dsn = TRUE)
+
