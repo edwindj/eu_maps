3696e3b645a6ce709c13c7b2a1e1cc86de9238b7
Edwin de Jonge <edwindjonge@gmail.com>
2019-04-12 08:30:23 +0200

    Update eu_ne_map.sh
    fix path

--- data-raw/eu_ne_map.sh
+++ data-raw/eu_ne_map.sh
@@ -2,7 +2,7 @@
   curl -O http://naciscdn.org/naturalearth/10m/cultural/ne_10m_admin_0_countries_lakes.zip &&
   unzip -o data-raw/ne_10m_admin_0_countries_lakes.zip -d data-raw/ne_admin
 
-rm -rf ../data
+rm -rf ./data
 mkdir -p ./data
 
 mapshaper -i data-raw/ne_admin/ne_10m_admin_0_countries_lakes.shp \
